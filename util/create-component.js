9401f2a2450a2d3dd18e87a17c712f30348f2523
Maximiliano Delgado <mdelgado@baufest.com>
2021-03-26 01:37:34 -0300

    chore: create component with script

--- util/create-component.js
+++ util/create-component.js
@@ -1,4 +1,3 @@
-require("colors");
 const fs = require("fs");
 const templates = require("./templates");
 const signale = require('signale-logger');
@@ -7,16 +6,16 @@ const signale = require('signale-logger');
 const componentName = process.argv[2];
 
 if (!componentName) {
-    signale.error("Please supply a valid component name".red);
+    signale.error("Please supply a valid component name");
     process.exit(1);
 }
 
-signale.log("Creating Component Templates with name: " + componentName);
+signale.note(`Creating Component Templates with name: ${componentName}`);
 
 const componentDirectory = `./src/${componentName}`;
 
 if (fs.existsSync(componentDirectory)) {
-    signale.error(`Component ${componentName} already exists.`.red);
+    signale.error(`Component ${componentName} already exists.`);
     process.exit(1);
 }
 
@@ -25,12 +24,9 @@ fs.mkdirSync(componentDirectory);
 const generatedTemplates = templates.map((template) => template(componentName));
 
 generatedTemplates.forEach((template) => {
-    fs.writeFileSync(
-        `${componentDirectory}/${componentName}${template.extension}`,
-        template.content
-    );
+    fs.writeFileSync(`${componentDirectory}/${template.filename}${template.extension}`, template.content);
 });
 
-signale.log(
-    "Successfully created component under: " + componentDirectory.green
-);
+fs.appendFileSync('./src/index.ts', `export * from './${componentName}';`);
+
+signale.success(`Successfully created component under: ${componentDirectory}`);
