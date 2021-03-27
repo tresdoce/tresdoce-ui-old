1e4b19d7eef2d3c94f3688697daa906bcc91d441
Maximiliano Delgado <mdelgado@baufest.com>
2021-03-27 14:11:40 -0300

    chore: refactor main storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,52 +13,63 @@ module.exports = {
     '@storybook/addons',
   ],
   webpackFinal: async (config) => {
-    config.module.rules.push(
-      {
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [['react-app', { flow: false, typescript: true }]],
-        },
-      }, {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../', 'packages/**/*'),
-      }
-      , {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader',
-        ],
-        include: path.resolve(__dirname, '../', 'packages/**/*'),
-      }, {
-        test: /\.css$/,
-        use: [
-          'style-loader',
+    config = {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
           {
-            loader: 'css-loader',
+            test: /\.(ts|tsx)$/,
+            loader: require.resolve('babel-loader'),
             options: {
-              importLoaders: 1,
+              presets: [['react-app', { flow: false, typescript: true }]],
             },
           },
-          'postcss-loader',
-        ],
-        include: path.resolve(__dirname, '../', 'packages/**/*'),
-      },
-      {
-        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 900000,
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: path.resolve(__dirname, '../', 'packages/**/*'),
           },
-        },
-        include: path.resolve(__dirname, '../', 'packages/**/*'),
-      });
-
-    config.resolve.extensions.push('.ts', '.tsx');
+          {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader'],
+            include: path.resolve(__dirname, '../', 'packages/**/*'),
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              'postcss-loader',
+            ],
+            include: path.resolve(__dirname, '../', 'packages/**/*'),
+          },
+          {
+            test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 900000,
+              },
+            },
+            include: path.resolve(__dirname, '../', 'packages/**/*'),
+          },
+        ],
+      },
+      resolve: {
+        ...config.resolve,
+        extensions: [
+          ...config.resolve.extensions,
+          '.ts', '.tsx',
+        ],
+      },
+    };
     /*config.resolve.alias = {
       ...config.resolve.alias,
       '@tresdoce-ui/core': path.resolve(__dirname,'../','packages/core/src')
