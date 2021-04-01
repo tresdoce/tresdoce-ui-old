073e1456719db4827e68a82304af641dbb795da0
Maximiliano Delgado <mdelgado@baufest.com>
2021-04-01 20:43:26 -0300

    chore: work with storybook

--- packages/core/src/components/Button/Button.tsx
+++ packages/core/src/components/Button/Button.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ButtonProps } from "./Button.types";
 import {ButtonStyle} from './styles.jsx';
 
-
 const Button: React.FC<ButtonProps> = ({ foo, onClick }) => (
     <ButtonStyle onClick={onClick} data-testid="Button" className="foo-bar">{foo}</ButtonStyle>
 );
