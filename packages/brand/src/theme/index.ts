a791505eb501914390364112362f98083b84c4c0
Maximiliano Delgado <mdelgado@baufest.com>
2021-04-11 17:16:29 -0300

    chore: add assets images and fonts

--- packages/brand/src/theme/index.ts
+++ packages/brand/src/theme/index.ts
@@ -1,12 +1,14 @@
 import createPalette, { Palette, PaletteInput } from './create-palette';
 import createTypography, { Typography, TypographyInput } from './create-typography';
 import zIndex, { ZIndex } from './z-index';
+import themeImages, {Image} from './images';
 import spacing, { Spacing } from './spacing';
 
 export interface Theme {
   palette: Palette,
   typography: Typography,
   spacing: Spacing,
+  images: Image,
   zIndex: ZIndex
 }
 
@@ -23,11 +25,13 @@ const createTheme = (options: ThemeInput): Theme => {
 
   const palette = createPalette(paletteInput);
   const typography = createTypography(typographyInput);
+  const images = themeImages();
 
   return {
     palette,
     spacing,
     typography,
+    images,
     zIndex,
   };
 };
