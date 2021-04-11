7e41e0a0542c0468db67e08cf7e0b81f5b248879
Maximiliano Delgado <mdelgado@baufest.com>
2021-04-11 20:33:39 -0300

    chore: add font sizes

--- packages/brand/src/theme/create-typography.ts
+++ packages/brand/src/theme/create-typography.ts
@@ -6,6 +6,7 @@ export interface Typography {
   fontSizeH4: string;
   fontSizeH5: string;
   fontSizeH6: string;
+  fontSizeP: string;
   fontSizeButton: string;
   fontWeightNormal: number;
   fontWeightLight: number;
@@ -17,15 +18,40 @@ export type TypographyInput = { +readonly [K in keyof Typography]+?: Typography[
   htmlFontSize?: number;
 };
 
-const defaultFontFamily = '\'DINNextLTPro-Condensed\', sansSerif';
+const defaultFontFamily = '\'DINNextLTPro-Condensed\', Helvetica, Arial, sans-serif';
+
+const size = {
+  px8: 8,
+  px10: 10,
+  px12: 12,
+  px14: 14,
+  px16: 16,
+  px18: 18,
+  px20: 20,
+  px24: 24,
+  px28: 28,
+  px30: 30,
+  px32: 32,
+  px36: 36,
+  px42: 42,
+  px48: 48,
+  px54: 54,
+  px60: 60,
+  px64: 64,
+  px68: 68,
+  px76: 76,
+  px84: 84,
+  px92: 92,
+  px96: 96,
+}
 
 const createTypography = (typography: TypographyInput): Typography => {
   const {
     fontFamily = defaultFontFamily,
 
     /* sizes */
-    fontSize = 14,
-    htmlFontSize = 16,
+    fontSize = size.px14,
+    htmlFontSize = size.px16,
 
     /* weights */
     fontWeightNormal = 400,
@@ -33,18 +59,19 @@ const createTypography = (typography: TypographyInput): Typography => {
     fontWeightBold = 700,
   } = typography;
 
-  const coef = fontSize / 14;
+  const coef = fontSize / size.px14;
   const pxToRem = (size: number) => `${(size / htmlFontSize) * coef}rem`;
 
   return {
     fontFamily,
-    fontSizeH1: pxToRem(96),
-    fontSizeH2: pxToRem(60),
-    fontSizeH3: pxToRem(48),
-    fontSizeH4: pxToRem(34),
-    fontSizeH5: pxToRem(24),
-    fontSizeH6: pxToRem(20),
-    fontSizeButton: pxToRem(14),
+    fontSizeH1: pxToRem(size.px42),
+    fontSizeH2: pxToRem(size.px32),
+    fontSizeH3: pxToRem(size.px28),
+    fontSizeH4: pxToRem(size.px24),
+    fontSizeH5: pxToRem(size.px20),
+    fontSizeH6: pxToRem(size.px16),
+    fontSizeP: pxToRem(size.px16),
+    fontSizeButton: pxToRem(size.px14),
     fontWeightNormal,
     fontWeightLight,
     fontWeightBold,
