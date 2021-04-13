b490500a80b88f3979e5836b301d69bb190cd126
Maximiliano Delgado <mdelgado@baufest.com>
2021-04-12 23:47:24 -0300

    chore: refactor pallete to dinamyc colors

--- packages/brand/src/theme/create-palette.ts
+++ packages/brand/src/theme/create-palette.ts
@@ -1,66 +1,34 @@
 import _ from 'lodash';
 
 export interface Palette {
-  white: string;
-  grey: string;
-  black: string;
-  brown: string;
-  pink: string;
-  purple: string;
-  violet: string;
-  blue: string;
-  teal: string;
-  green: string;
-  olive: string;
-  yellow: string;
-  orange: string;
-  red: string;
-  primary: string;
-  secondary: string;
-};
-
-export type PaletteInput = {
-  +readonly [K in keyof Palette]+?: Palette[K];
+  [name:string]:string;
 }
 
-const createPalette = (palette: PaletteInput): Palette => {
-  const {
-    white = '#fff',
-    grey = '#767676',
-    black = '#1B1C1D',
-    brown = '#A5673F',
-    pink = '#E03997',
-    purple = '#A333C8',
-    violet = '#6435C9',
-    blue = '#2185D0',
-    teal = '#00B5AD',
-    green = '#21BA45',
-    olive = '#B5CC18',
-    yellow = '#FBBD08',
-    orange = '#F2711C',
-    red = '#DB2828',
-    primary = '#009688',
-    secondary = '#07C9B7',
-  } = palette;
+export type PaletteInput = {
+  [name: string]: string;
+}
 
-  return {
-    white,
-    grey,
-    black,
-    brown,
-    pink,
-    purple,
-    violet,
-    blue,
-    teal,
-    green,
-    olive,
-    yellow,
-    orange,
-    red,
-    primary,
-    secondary
-  };
+const defaultPallete = {
+  white: '#fff',
+  grey: '#767676',
+  black: '#1B1C1D',
+  brown: '#A5673F',
+  pink: '#E03997',
+  purple: '#A333C8',
+  violet: '#6435C9',
+  blue: '#2185D0',
+  teal: '#00B5AD',
+  green: '#21BA45',
+  olive: '#B5CC18',
+  yellow: '#FBBD08',
+  orange: '#F2711C',
+  red: '#DB2828',
+  primary: '#009688',
+  secondary: '#07C9B7',
+};
+
+const createPalette = (palette?: PaletteInput): Palette => {
+  return _.merge(defaultPallete, palette);
 };
 
 export default createPalette;
