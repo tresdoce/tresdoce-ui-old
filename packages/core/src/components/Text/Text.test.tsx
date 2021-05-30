823ec34bf1cfad89b0cc0e24493bd44de5f45a7f
Maximiliano Delgado <mdelgado@baufest.com>
2021-05-30 02:02:05 -0300

    refactor: all test text component

--- packages/core/src/components/Text/Text.test.tsx
+++ packages/core/src/components/Text/Text.test.tsx
@@ -25,7 +25,7 @@ describe('Component - Text', () => {
   const renderComponent = () => render(<Layout theme={createTheme()} cdnBasepath={''}><Text {...props} /></Layout>);
   const updateComponent = (renderer, props) => renderer(<Layout theme={createTheme()} cdnBasepath={''}><Text {...props} /></Layout>);
 
-  it('Should be render component text', () => {
+  it('Should be render text component', () => {
     props['data-testId'] = 'id-test-component-text';
 
     const { getByTestId } = renderComponent();
@@ -35,7 +35,7 @@ describe('Component - Text', () => {
     expect(component).toHaveTextContent(props.children);
   });
 
-  it('Should be render component text align left, center, right and justify', () => {
+  it('Should be render text component align left, center, right and justify', () => {
     props['data-testId'] = 'id-test-component-text';
     props.align = 'left';
 
@@ -45,16 +45,122 @@ describe('Component - Text', () => {
     expect(component).toHaveStyle(`text-align: ${props.align}`);
 
     props.align = 'center';
-    updateComponent(rerender,props);
+    updateComponent(rerender, props);
     expect(component).toHaveStyle(`text-align: ${props.align}`);
 
     props.align = 'right';
-    updateComponent(rerender,props);
+    updateComponent(rerender, props);
     expect(component).toHaveStyle(`text-align: ${props.align}`);
 
     props.align = 'justify';
-    updateComponent(rerender,props);
+    updateComponent(rerender, props);
     expect(component).toHaveStyle(`text-align: ${props.align}`);
+  });
+
+  it('Should be render text component with different color', () => {
+    props['data-testId'] = 'id-test-component-text';
+    props.color = '#1B1C1D';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`color: ${props.color}`);
+
+    props.color = '#34354A';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`color: ${props.color}`);
+  });
+
+  it('Should be render text component with different size', () => {
+    props['data-testId'] = 'id-test-component-text';
+    props.size = '0.875rem';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`font-size: ${props.size}`);
+
+    props.size = '16px';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-size: ${props.size}`);
+  });
+
+  it('Should be render text component with different font style', () => {
+    props['data-testId'] = 'id-test-component-text';
+    props.fontStyle = 'initial';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`font-style: ${props.fontStyle}`);
+
+    props.fontStyle = 'normal';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-style: ${props.fontStyle}`);
+
+    props.fontStyle = 'italic';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-style: ${props.fontStyle}`);
+  });
+
+  it('Should be render text component with different font weight', () => {
+    props['data-testId'] = 'id-test-component-text';
+    props.weight = 'normal';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`font-weight: ${props.weight}`);
+
+    props.weight = 'thin';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 100`);
+
+    props.weight = 'light';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 300`);
+
+    props.weight = 'regular';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 400`);
+
+    props.weight = 'medium';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 500`);
+
+    props.weight = 'bold';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 700`);
+
+    props.weight = 'black';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`font-weight: 900`);
+  });
+
+  it('Should be render text component with different capitalize', () => {
+    props['data-testId'] = 'id-test-component-text';
+    props.capitalize = 'initial';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`text-transform: ${props.capitalize}`);
+
+    props.capitalize = 'capitalize';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`text-transform: ${props.capitalize}`);
+
+    props.capitalize = 'lowercase';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`text-transform: ${props.capitalize}`);
+
+    props.capitalize = 'uppercase';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`text-transform: ${props.capitalize}`);
+
+    props.capitalize = 'first-letter';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`text-transform: ${props.capitalize}`);
   });
 
 });
