9f39eadf4ee1ae41d1a582f67fde513edc5f8fec
Maximiliano Delgado <mdelgado@baufest.com>
2021-06-27 18:01:04 -0300

    chore: add test container component

--- packages/core/src/components/Container/Container.test.tsx
+++ packages/core/src/components/Container/Container.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
-import { createTheme } from '../../../../brand/src';
+import { createTheme, getSizeValue } from '@tresdoce-ui/brand';
 import Layout from '../Layout';
 
 import Container from "./Container";
@@ -8,33 +8,108 @@ import { ContainerProps } from "./Container.types";
 
 describe("Component - Container", () => {
   let props: ContainerProps;
+  const theme = createTheme();
+
+  const getSpacing = (theme,gutter = 'md') => getSizeValue({ size: gutter, sizes: theme.grid.spacing.gutter});
 
   beforeEach(() => {
     props = {
       className: '',
-      children: "bar",
+      children: "This is a content",
       row: false,
       fluid: false,
       size: "md",
     };
   });
 
-  const renderComponent = () => render(<Layout theme={createTheme()} cdnBasepath={''}><Container {...props} /></Layout>);
-  const updateComponent = (renderer, props) => renderer(<Layout theme={createTheme()} cdnBasepath={''}><Container {...props} /></Layout>);
+  const renderComponent = () => render(<Layout theme={theme} cdnBasepath={''}><Container {...props} /></Layout>);
+  const updateComponent = (renderer, props) => renderer(<Layout theme={theme} cdnBasepath={''}><Container {...props} /></Layout>);
 
   it('Should has a correct displayName', () =>{
     expect(Container.displayName).toEqual('@tresdoce-ui/core/Container');
   });
 
-  /*it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    props['data-testId'] = "id-Container";
-    const { getByTestId, rerender } = renderComponent();
-    const component = getByTestId(props['data-testId']);
-    expect(component).toHaveTextContent(props.foo);
+  it('Should be render Container component', () => {
+    props['data-testId'] = 'id-test-container-component';
 
-    props.foo = 'harvey was here 2';
+    const { getByTestId } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    const component = getByTestId(props['data-testId']);
+    expect(component.tagName).toEqual(`DIV`);
+    expect(component.firstChild.textContent).toEqual(props.children)
+  });
+
+  it('Should be render Container component fluid and not fluid', () => {
+    props['data-testId'] = 'id-test-container-component';
+    props['fluid'] = false;
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    let component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+
+    props['fluid'] = true;
     updateComponent(rerender, props);
-    expect(component).toHaveTextContent(props.foo);
-  });*/
+    expect(component).toHaveStyle(`width: auto`);
+    expect(component).toHaveStyle(`max-width: 100%`);
+  });
+
+  it('Should be render Container component with different max width', () => {
+    props['data-testId'] = 'id-test-container-component';
+    props['fluid'] = false;
+    props['size'] = 'md';
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    let component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['size'] = 'xs';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['size'] = 'sm';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['size'] = 'lg';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['size'] = 'xl';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['size'] = 'xxl';
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`width: 100%`);
+    expect(component).toHaveStyle(`max-width: ${theme.sizing.mqSizes[props.size].maxWidth}`);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+  });
+
+  it('Should be render Container component with row and without row', () => {
+    props['data-testId'] = 'id-test-container-component';
+    props['row'] = false;
+
+    const { getByTestId, rerender } = renderComponent();
+    expect(getByTestId).not.toBeNull();
+    let component = getByTestId(props['data-testId']);
+    expect(component).toHaveStyle(`padding: 0 ${getSpacing(theme, props.size)}px`);
+
+    props['row'] = true;
+    updateComponent(rerender, props);
+    expect(component).toHaveStyle(`padding: 0 0`);
+  })
 });
